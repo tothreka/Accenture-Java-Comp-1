a744ade60c9be9373f05ac41f6dda75d96006f19
toth.reka <retoth95@gmail.com>
2020-06-11 17:01:53 +0200

    Initial commit

---
+++ Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/Accenture/Java/Comp1/AccentureJavaComp1Application.java
@@ -0,0 +1,13 @@
+package com.rekadanilaci.Accenture.Java.Comp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureJavaComp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentureJavaComp1Application.class, args);
+	}
+
+}
---
+++ Accenture-Java-Comp-1/src/test/java/com/rekadanilaci/Accenture/Java/Comp1/AccentureJavaComp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rekadanilaci.Accenture.Java.Comp1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccentureJavaComp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
