104524f817d9735dfdfca7028d557c58f4357676
toth.reka <retoth95@gmail.com>
2020-06-12 10:04:20 +0200

    fill employee, employeedto classes

--- Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/Accenture/domain/Employee.java
+++ Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/Accenture/domain/Employee.java
@@ -1,7 +1,19 @@
 package com.rekadanilaci.Accenture.domain;
 
-import javax.persistence.Entity;
+import com.rekadanilaci.Accenture.dto.EmployeeDto;
+
+import javax.persistence.*;
 
 @Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Employee(EmployeeDto employeeDto) {
+        this.name = employeeDto.getName();
+    }
 }
--- Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/Accenture/dto/EmployeeDto.java
+++ Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/Accenture/dto/EmployeeDto.java
@@ -1,4 +1,14 @@
 package com.rekadanilaci.Accenture.dto;
 
 public class EmployeeDto {
+    private String name;
+
+    public EmployeeDto(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+        return name;
+    }
 }
