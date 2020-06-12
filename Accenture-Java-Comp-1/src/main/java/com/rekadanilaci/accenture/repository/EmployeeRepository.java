79ff10d43403f959e229382b75fae8a762420644
daniel.pataki <P0k3m0n123>
2020-06-12 14:21:38 +0200

    Test

--- Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/repository/EmployeeRepository.java
+++ Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.rekadanilaci.accenture.repository;
 
 import com.rekadanilaci.accenture.domain.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
--- Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/repository/ReservationRepository.java
+++ Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/repository/ReservationRepository.java
@@ -2,6 +2,8 @@ package com.rekadanilaci.accenture.repository;
 
 import com.rekadanilaci.accenture.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
