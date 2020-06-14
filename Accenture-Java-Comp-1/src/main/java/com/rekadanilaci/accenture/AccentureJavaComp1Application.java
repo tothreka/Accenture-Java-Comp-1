1898d2cb44368dfb08b007d2ce102cfa5cfd841d
PalLaszlo <polip73@gmail.com>
2020-06-14 10:47:38 +0200

    add javadoc to Main

--- Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/AccentureJavaComp1Application.java
+++ Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/AccentureJavaComp1Application.java
@@ -3,6 +3,18 @@ package com.rekadanilaci.accenture;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The goal of the project is to create a Backend that can serve as part of
+ * a complex application used for managing the occupation of the office
+ * after the COVID pandemic.
+ *
+ * The employees must be able to send their reservations to the
+ * application and check the status of their reservations.
+ *
+ * The application will decide, who can enter the office, thus it must be
+ * integrated with the existing check-in system.
+ */
+
 @SpringBootApplication
 public class AccentureJavaComp1Application {
 
