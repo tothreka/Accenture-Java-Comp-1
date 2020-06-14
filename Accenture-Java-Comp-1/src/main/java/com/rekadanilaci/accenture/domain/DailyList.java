a52aa85fcb4e558be37df15cfd1990299ed67270
PalLaszlo <polip73@gmail.com>
2020-06-14 13:25:16 +0200

    add javadoc to DailyList

--- Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/domain/DailyList.java
+++ Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/domain/DailyList.java
@@ -6,6 +6,23 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Office class has a Map that contains instances of DailyList as values.
+ * The keys are the requested days, when the employees want to enter the office.
+ * The DailyLists contains all Reservations created for the given day. Because
+ * Reservations are immediately ordered to a DailyList after their creations,
+ * the DailyLists contain the Reservations in order pf creations.
+ *
+ * When a Reservation is used, it will remain in the appropriate DailyList,
+ * only the status of the Reservation will be changed.
+ *
+ * The appropriate DailyList will be created by the application automatically
+ * with the first created Reservation for the given day.
+ *
+ * DailyLists will be never deleted, allowing backtracking of the entries and
+ * leaves of the office.
+ */
+
 @Component
 @Entity
 @Table(name = "dailyList")
