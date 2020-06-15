4c40278d8e224607c3dc3efe027aca1750a50b8c
PalLaszlo <polip73@gmail.com>
2020-06-15 19:00:37 +0200

    refactor ReservationStatus

--- Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/dto/EmployeeDto.java
+++ Accenture-Java-Comp-1/src/main/java/com/rekadanilaci/accenture/dto/EmployeeDto.java
@@ -1,12 +1,14 @@
 package com.rekadanilaci.accenture.dto;
 
 public class EmployeeDto {
+
     private String name;
 
     public EmployeeDto(String name) {
         this.name = name;
     }
 
+    //================== GETTER =======================
 
     public String getName() {
         return name;
