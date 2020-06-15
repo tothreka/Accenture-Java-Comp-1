64d5ae82e80a3f1b18fdede901656d0341ef891e
daniel.pataki <P0k3m0n123>
2020-06-15 12:40:31 +0200

    Make OfficeManagementServiceTest tests

--- Accenture-Java-Comp-1/src/test/java/com/rekadanilaci/accenture/OfficeManagementServiceTest.java
+++ Accenture-Java-Comp-1/src/test/java/com/rekadanilaci/accenture/OfficeManagementServiceTest.java
@@ -8,12 +8,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.event.annotation.BeforeTestClass;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class OfficeManagementServiceTest {
 
     @Autowired
