fe5d9aeac4a7698f442127dc9e49ff2811462da2
toth.reka <retoth95@gmail.com>
2020-07-07 15:29:37 +0200

    load employee data in employee main page

--- Accenture-Java-Comp-1/frontend/src/app/components/employee-main/employee-main.component.ts
+++ Accenture-Java-Comp-1/frontend/src/app/components/employee-main/employee-main.component.ts
@@ -4,6 +4,8 @@ import {OfficeService} from "../../services/office.service";
 import {FormControl, FormGroup} from "@angular/forms";
 import {NewReservationModel} from "../../models/newReservation.model";
 import {ReservationService} from "../../services/reservation.service";
+import {ActivatedRoute} from "@angular/router";
+import {ReservationItemModel} from "../../models/reservationItem.model";
 
 @Component({
   selector: 'app-employee-main',
@@ -15,8 +17,9 @@ export class EmployeeMainComponent implements OnInit {
   showReservationForm: boolean = false;
   newReservationForm: FormGroup;
   newReservation: NewReservationModel;
+  private employeeId: number;
 
-  constructor(private officeService: OfficeService, private reservationService: ReservationService) {
+  constructor(private officeService: OfficeService, private reservationService: ReservationService, private activatedRoute: ActivatedRoute) {
     this.newReservationForm = new FormGroup({
       'day': new FormControl('')
     });
@@ -24,7 +27,29 @@ export class EmployeeMainComponent implements OnInit {
 
   ngOnInit(): void {
     this.employee = JSON.parse(localStorage.getItem('employee'));
+    this.employeeId = this.employee.id;
+    this.fetchEmployeeData(this.employeeId);
   }
+
+  fetchEmployeeData(id: number) {
+    this.officeService.fetchEmployeeData(id).subscribe(
+      data => {
+        this.employee = data;
+      },
+      error => {
+        console.log(error)
+      }
+    );
+    this.reservationService.fetchReservationsOfCurrentEmployee(this.employeeId).subscribe(
+      reservationList => {
+        this.employee.reservationList = <Array<ReservationItemModel>>reservationList;
+      },
+      error => {
+        console.log(error)
+      }
+    )
+  }
+
 
   createNewReservation() {
     this.newReservation = {
@@ -36,5 +61,9 @@ export class EmployeeMainComponent implements OnInit {
         this.employee.reservationList.push(reservationItem);
       }
     )
+  }
+
+  showChangeIcons() {
+
   }
 }
